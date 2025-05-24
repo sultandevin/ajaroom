f4747af3f4a45d9b7c06a0a3a0803e7b370f7f10
Devin <sultandevinosuyudi@mail.ugm.ac.id>
2025-05-25 02:36:05 +0700

    fix(routes): add async to every function name

--- server/room-service/app/routes/booking.py
+++ server/room-service/app/routes/booking.py
@@ -10,14 +10,14 @@ SessionDep = Annotated[Session, Depends(get_session)]
 
 
 @router.get("/")
-def read_booking(
+async def read_booking(
     session: SessionDep, start: int = 1, end: Annotated[int, Query(le=100)] = 10
 ) -> list[Booking]:
     return session.exec(select(Booking).offset(start - 1).limit(end - start + 1)).all()
 
 
 @router.post("/", response_model=Booking)
-def create_booking(booking: Booking, session: SessionDep) -> Booking:
+async def create_booking(booking: Booking, session: SessionDep) -> Booking:
     booking = Booking(
         start_time=isoparse(str(booking.start_time)),
         end_time=isoparse(str(booking.end_time)),
@@ -28,8 +28,9 @@ def create_booking(booking: Booking, session: SessionDep) -> Booking:
     session.refresh(booking)
     return booking
 
+
 @router.get("/{booking_id}")
-def read_bookin(booking_id: int, session: SessionDep) -> Booking:
+async def read_booking(booking_id: int, session: SessionDep) -> Booking:
     booking = session.get(Booking, booking_id)
     if not booking:
         raise HTTPException(status_code=404, detail="Booking not found")
@@ -37,10 +38,10 @@ def read_bookin(booking_id: int, session: SessionDep) -> Booking:
 
 
 @router.delete("/{booking_id}")
-def delete_booking(booking_id: int, session: SessionDep):
+async def delete_booking(booking_id: int, session: SessionDep):
     booking = session.get(Booking, booking_id)
     if not booking:
         raise HTTPException(status_code=404, detail="Booking not found")
-    session.delete(booking )
+    session.delete(booking)
     session.commit()
     return {"ok": True, "message": f"Booking {booking_id} deleted successfully"}
