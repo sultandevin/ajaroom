b4ef51de7c38c74359a713e2609bf7ceb0b3e268
Devin <sultandevinosuyudi@mail.ugm.ac.id>
2025-05-25 02:26:40 +0700

    fix(model): add fields to room model

--- server/room-service/app/models/room.py
+++ server/room-service/app/models/room.py
@@ -4,5 +4,7 @@ from sqlmodel import Field, SQLModel
 class Room(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     name: str = Field(index=True, max_length=100)
+    location: str = Field(index=True, max_length=200)
     capacity: int = Field(index=True)
+    description: str = Field(default=None, max_length=500)
     available: bool = Field(default=True)
